e4ab8bf394050a856419817ad688ef5b19548698
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-22 17:34:24 -0400

    Fixed all of the author.id stuff

--- src/main/kotlin/com/lonnondev/examplemod/ExampleMod.kt
+++ src/main/kotlin/com/lonnondev/examplemod/ExampleMod.kt
@@ -1,10 +1,10 @@
-package com.mod_author.mod_id
+package com.lonnondev.examplemod
 
 import net.fabricmc.api.ModInitializer
 
 @Suppress("UNUSED")
-object ModName: ModInitializer {
-    private const val MOD_ID = "mod_id"
+object ExampleMod: ModInitializer {
+    private const val MOD_ID = "examplemod"
     override fun onInitialize() {
         println("Example mod has been initialized.")
     }
